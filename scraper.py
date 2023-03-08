d6e8407f5be42f0e34c757c20789b81bac8a3935
abuh1 <abuhamza2308@hotmail.com>
2023-03-08 18:56:02 +0000

    update

--- scraper.py
+++ scraper.py
@@ -12,11 +12,6 @@ def write_json(data, filename='draftdex.json'):
         f.seek(0)
         json.dump(file_data, f, indent = 4)
         
-# Function to scrape detailed pages of each pokemon, including alternate forms
-def scrape_details(pokemon):
-    pass
-        
-        
 path = './'
 fileName = 'test'
 
@@ -27,33 +22,34 @@ soup = BeautifulSoup(result.text, "html.parser")
 
 entries = soup.find_all("table", id="pokedex")[0].find_all("tbody")[0].find_all("tr")
 
-for i, pokemon in enumerate(entries[0:20]):
+for i, pokemon in enumerate(entries[69:70]):
+    varnames = ('id', 'dexno', 'name', 'height', 'weight', 'image', 'types', 'hp', 'atk', 'defense','spatk',
+                'spdef', 'spd', 'total')
     data = {}
     # Creates list of every piece of data of the pokemon
     info = pokemon.find_all("td")
     
     # Unique index for each data entry
-    data['id'] = i
+    id = i
     # Pokedex number for each pokemon. Not unique (variations of the same pokemon have the same dex number)
-    data['dexno'] = int(info[0]['data-sort-value'])
+    dexno = int(info[0]['data-sort-value'])
     # Gets name of pokemon, but if pokemon is a different variation it will replace the name with the variation name. e.g. Mega Venusaur
-    data['name'] = info[1].find_all("a")[0].getText()
+    name = info[1].find_all("a")[0].getText()
     if info[1].find_all("small"):
-        data['name'] = info[1].find_all("small")[0].getText()
+        name = info[1].find_all("small")[0].getText()
     # Puts types into list
     types = []
     for t in info[2].find_all("a"):
         types.append(t.getText())
-    data['types'] = types
     
     # Scrape stats
-    data['hp'] = info[4].getText()
-    data['atk'] = info[5].getText()
-    data['defense'] = info[6].getText()
-    data['spatk'] = info[7].getText()
-    data['spdef'] = info[8].getText()
-    data['spd'] = info[9].getText()
-    data['total'] = info[3].getText()
+    hp = info[4].getText()
+    atk = info[5].getText()
+    defense = info[6].getText()
+    spatk = info[7].getText()
+    spdef = info[8].getText()
+    spd = info[9].getText()
+    total = info[3].getText()
     
     # Start to scrape the individual page of each pokemon for additional data
     details_ext = info[1].find_all("a")[0]["href"]
@@ -61,12 +57,35 @@ for i, pokemon in enumerate(entries[0:20]):
     # e.g https://pokemondb.net/pokedex/bulbasaur
     details_url = f"https://pokemondb.net{details_ext}"
     page = requests.get(details_url)
-    det_soup = BeautifulSoup(page.text, 'html.parser')
+    details_soup = BeautifulSoup(page.text, 'html.parser')
     
-    # Get image - gets alternate form image if other forms
-    images = det_soup.find_all("picture")[1].find_all("img")
-    for img in images:
-        pic = img['src']
+    # Get each tab name in a list
+    tab_names = []
+    tab_labels = details_soup.find_all(class_ = "sv-tabs-tab-list")[0].find_all(class_ = "sv-tabs-tab")
+    for label in tab_labels:
+        tab_names.append(label.getText())
     
-    print(data['name'])
-    print(pic)
+    # Get each tab panel in a list
+    info_tabs = details_soup.find_all(class_ = "tabset-basics")[0].find_all(class_ = "sv-tabs-panel-list")[0].find_all(class_ = "sv-tabs-panel")
+    
+    # Choose current tab info for the pokemon
+    zipped_tabs = list(zip(tab_names, info_tabs))
+    for tabs in zipped_tabs:
+        if tabs[0] == name:
+            active = tabs[1]
+    # Scrape the rest of the info from active tab
+    height = active.find_all(class_ = "vitals-table")[0].find_all("tr")[3].find("td").getText()
+    weight = active.find_all(class_ = "vitals-table")[0].find_all("tr")[4].find("td").getText()
+    image = active.find_all("picture")[0].find_all("img")[0]["src"]
+    abilities = active.find_all(class_ = "vitals-table")[0].find_all("tr")[5].find(class_ = "text-muted")
+    
+    print(name)
+    print(image)    
+    
+    
+    # Put all data into a dictionary
+    for i in varnames:
+        data[i] = locals()[i]
+    
+    # Output to json file
+    # write_json(data)
